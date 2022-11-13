a948005010b3caea58e35a62f364fe4719d91ea2
SBolsec <sinisa.bolsec0@gmail.com>
2022-11-13 19:54:45 +0100

    lab 2

--- lab-2/balancer_serv.erl
+++ lab-2/balancer_serv.erl
@@ -1,33 +1,69 @@
 -module(balancer_serv).
 -author("sbolsec").
 
-%% API
 -export([start_link/0, start_link/2]).
--export([add/1, delete/1, find/1, stop/0]).
-
-%%%===================================================================
-%%% API
-%%%===================================================================
+-export([add/1, delete/1, find/1, stop/0, stop_node/1]).
+-export([main/2]).
 
 start_link() ->
     start_link(number_serv, other_serv).
 
 start_link(NumberServ, OtherServ) ->
-    %% TODO: spawn servers
-    NumberServPid = 1,
-    OtherServerPid = 2,
-    BalancerPid = spawn(balancer_serv, main, [NumberServPid, OtherServerPid]),
+    NumberServPid = spawn(store_serv, start_link, [OtherServ, false]),
+    register(NumberServ, NumberServPid),
+    OtherServerPid = spawn(store_serv, start_link, [NumberServ, false]),
+    register(OtherServ, OtherServerPid),
+    BalancerPid = spawn(balancer_serv, main, [NumberServ, OtherServ]),
     register(balancer, BalancerPid).
 
-main(NumberServPid, OtherServerPid) ->
+main(NumberServ, OtherServ) ->
     receive
-        {From, {Action, Value}} ->
-            1,
-            main(NumberServPid, OtherServerPid)
+        {_From, stop} ->
+            NumberServ ! {self(), stop},
+            OtherServ ! {self(), stop},
+            stoped;
+        {_From, {stop_node, ServName}} ->
+            ServName ! {self(), stop},
+            main(NumberServ, OtherServ);
+        {From, {find, Value}} ->
+            send({NumberServ, OtherServ}, {find, Value}),
+            receive
+                Result -> From ! Result
+            end,
+            main(NumberServ, OtherServ);
+        {_From, {Action, Value}} ->
+            send({NumberServ, OtherServ}, {Action, Value}),
+            main(NumberServ, OtherServ)
+    end.
+
+send({NumberServ, OtherServ}, {Action, Value}) ->
+    check_servers(NumberServ, OtherServ),
+    if
+        is_integer(Value) ->
+            NumberServ ! {self(), {Action, Value}};
+        true ->
+            OtherServ ! {self(), {Action, Value}}
+    end.
+
+check_servers(NumberServ, OtherServ) ->
+    check_server(NumberServ, OtherServ),
+    check_server(OtherServ, NumberServ).
+
+check_server(Master, Slave) ->
+    io:format("Checking server ~w, process: ~w~n", [Master, self()]),
+    Pid = whereis(Master),
+    if
+        Pid == undefined ->
+            io:format("Server ~w is down, restarting it, process: ~w~n", [Master, self()]),
+            NewPid = spawn(store_serv, start_link, [Slave, true]),
+            register(Master, NewPid);
+        true ->
+            io:format("Server ~w is ok, process: ~w~n", [Master, self()]),
+            ok
     end.
 
 start_client(stop) ->
-    1;
+    balancer ! {self(), stop};
 start_client({find, Value}) ->
     balancer ! {self(), {find, Value}},
     receive
@@ -47,3 +83,6 @@ find(Value) ->
 
 stop() ->
     start_client(stop).
+
+stop_node(ServName) ->
+    start_client({stop_node, ServName}).
--- lab-2/store_serv.erl
+++ lab-2/store_serv.erl
@@ -1,20 +1,95 @@
 -module(store_serv).
 -author("sbolsec").
 
-%% API
 -export([start_link/2]).
 
-start_link(Master, Slave) ->
-    1.
+start_link(Slave, false) ->
+    io:format("Initializing process first time with slave ~w, process: ~w~n", [Slave, self()]),
+    main([store_log:create(), store_log:create(), Slave]);
+start_link(Slave, true) ->
+    main(init(Slave)).
 
-main() ->
+init(Slave) ->
+    io:format("Initializing process with slave: ~w, process: ~w ~n", [Slave, self()]),
+    Pid = whereis(Slave),
+    if
+        Pid == undefined ->
+            io:format("Slave ~w is not running, creating initial empty state, process: ~w~n", [
+                Slave, self()
+            ]),
+            [store_log:create(), store_log:create(), Slave];
+        true ->
+            io:format("Slave ~w is running, sending get_backup request, process: ~w~n", [
+                Slave, self()
+            ]),
+            Slave ! {self(), get_backup},
+            receive
+                [MainStore, BackupStore] ->
+                    io:format(
+                        "Received backup from slave ~w, main store: ~w, backup store: ~w, process: ~w~n",
+                        [Slave, MainStore, BackupStore, self()]
+                    ),
+                    [MainStore, BackupStore, Slave]
+            end
+    end.
+
+main(State) ->
     receive
-        {From, {add, Value}} ->
-            main();
-        {From, {delete, Value}} ->
-            main();
+        {_From, {add, Value}} ->
+            NewState = add(Value, State),
+            io:format("Added value: ~w, new state: ~w, process: ~w ~n", [Value, NewState, self()]),
+            main(NewState);
+        {_From, {add_backup, Value}} ->
+            NewState = add_backup(Value, State),
+            io:format("Added backup value: ~w, new state: ~w, process: ~w ~n", [
+                Value, NewState, self()
+            ]),
+            main(NewState);
+        {_From, {delete, Value}} ->
+            NewState = delete(Value, State),
+            io:format("Deleted value: ~w, new state: ~w, process: ~w ~n", [Value, NewState, self()]),
+            main(NewState);
+        {_From, {delete_backup, Value}} ->
+            NewState = delete_backup(Value, State),
+            io:format("Deleted backup value: ~w, new state: ~w, process: ~w ~n", [
+                Value, NewState, self()
+            ]),
+            main(NewState);
         {From, {find, Value}} ->
-            main();
-        {From, stop} ->
+            Reply = find(Value, State),
+            io:format("Find value: ~w, found: ~w, process: ~w ~n", [Value, Reply, self()]),
+            From ! Reply,
+            main(State);
+        {From, get_backup} ->
+            Reply = get_backup(State),
+            io:format("Get backup, reply is: ~w, process: ~w ~n", [Reply, self()]),
+            From ! Reply,
+            main(State);
+        {_From, stop} ->
+            io:format("Stoping... ~w ~n", [self()]),
             stoped
     end.
+
+add(Value, [MainStore, BackupStore, Slave]) ->
+    NewMainStore = store_log:add(MainStore, Value),
+    Slave ! {self(), {add_backup, Value}},
+    [NewMainStore, BackupStore, Slave].
+
+add_backup(Value, [MainStore, BackupStore, Slave]) ->
+    NewBackupStore = store_log:add(BackupStore, Value),
+    [MainStore, NewBackupStore, Slave].
+
+delete(Value, [MainStore, BackupStore, Slave]) ->
+    NewMainStore = store_log:delete(MainStore, Value),
+    Slave ! {self(), {delete_backup, Value}},
+    [NewMainStore, BackupStore, Slave].
+
+delete_backup(Value, [MainStore, BackupStore, Slave]) ->
+    NewBackupStore = store_log:delete(BackupStore, Value),
+    [MainStore, NewBackupStore, Slave].
+
+find(Value, [MainStore | _]) ->
+    store_log:find(MainStore, Value).
+
+get_backup([MainStore, BackupStore, _Slave]) ->
+    [BackupStore, MainStore].
