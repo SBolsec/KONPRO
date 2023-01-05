bb82ac96ffaff335ee8585b531aeac3d9a2c17aa
Siniša Bolšec <sinisa.bolsec0@gmail.com>
2023-01-05 22:10:35 +0100

    dz04 init spring boot app

---
+++ dz-04/src/main/java/com/sbolsec/dz04/Dz04Application.java
@@ -0,0 +1,13 @@
+package com.sbolsec.dz04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dz04Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dz04Application.class, args);
+    }
+
+}
